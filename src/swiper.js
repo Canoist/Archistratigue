42d575f89ca1c28ff074fb55c1cb60517d65a9d3
Leonid Lichkun <lionproklon@gmail.com>
2022-12-07 23:52:25 +0300

    Added swiper for video, fixed some styles (timestamp 15hr)

--- src/swiper.js
+++ src/swiper.js
@@ -1,11 +1,9 @@
 import Swiper, { Pagination, EffectFade, Autoplay } from "swiper";
 import "swiper/css";
 
-
 Swiper.use([Autoplay, Pagination, EffectFade]);
 
-
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".swiper-image", {
     loop: true,
     autoHeight: true,
     speed: 500,
@@ -14,11 +12,19 @@ const swiper = new Swiper(".swiper", {
         crossFade: true,
     },
     pagination: {
-        el: ".swiper-pagination",
+        el: ".swiper-pagination-image",
         clickable: true,
     },
     autoplay: {
         disableOnInteraction: false,
         waitForTransition: false,
+    },
+});
+
+const swiperVideo = new Swiper(".swiper-video", {
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination-video",
+        clickable: true,
     },
 });
